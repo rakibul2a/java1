eb44bf1bdf96161a118a40e2d1b4b7ace5adf0f6
Rakibul Islam <167644021+rakibul2a@users.noreply.github.com>
2024-08-08 16:01:45 +0600

    Add files via upload

---
+++ jss/script.js
@@ -0,0 +1,29 @@
+// emp1
+function showText(){
+	document.getElementById('s_text').innerHTML="Hello SSB"
+}
+
+// emp2
+function showName(){
+	document.getElementById('s_name').innerHTML="Rakibiul Islam"
+}
+
+// emp3
+function showLearn(){
+	document.getElementById('s_ques').innerHTML="Java Script"
+}
+
+// emp4
+function fontChange(){
+    document.getElementById('f_size').style.fontSize='70px'
+}
+
+// emp5
+function colorChange(){
+    document.getElementById('color_change').style.color='green'
+}
+
+// emp2
+function showMentor(){
+	document.getElementById('s_mentor').innerHTML="Minhaz Faisal"
+}
